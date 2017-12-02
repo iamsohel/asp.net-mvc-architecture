296ca7108dfc894db7545a4efa419b8b79c8895b
Sohel Rana <sohel9001@yahoo.com>
2017-12-02 07:45:36 +0600

    final commit

---
+++ Stores.Web/Mappings/DomainToViewModelMappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using Store.Model.Models;
+using Stores.Web.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+namespace Stores.Web.Mappings
+{
+    public class DomainToViewModelMappingProfile: Profile
+    {
+        public override string ProfileName
+        {
+            get { return "DomainToViewModelMappings"; }
+        }
+
+        protected override void Configure()
+        {
+            Mapper.CreateMap<Category, CategoryViewModel>();
+            Mapper.CreateMap<Gadget, GadgetViewModel>();
+        }
+    }
+}
